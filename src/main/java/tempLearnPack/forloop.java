21be9190fd4856e0e5f0308b1f52b225d4949b6f
Praveen <garigipati.praveenkumar@gmail.com>
2019-02-14 19:42:26 +0530

    added forloop.java file

---
+++ src/main/java/tempLearnPack/forloop.java
@@ -0,0 +1,16 @@
+package tempLearnPack;
+
+/**
+ * Hello world!
+ *
+ */
+public class forloop 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+		String iStr = sc.hasnext();
+		int i = Integer.ParseInt(iStr);
+		System.out.println("additional value"+(i+1));
+    }
+}
